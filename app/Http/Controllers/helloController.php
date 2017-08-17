d8cac7ad4bba1862e55d979c85e493e9310aa7fe
zyg <a1871270@163.com>
2017-08-17 10:02:34 +0800

    配置wechat SDK
    配置wechat SDK

--- app/Http/Controllers/helloController.php
+++ app/Http/Controllers/helloController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Console\Application;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,6 +11,16 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class helloController extends BaseController
 {
     public function index(){
+	    $wechat = new \EasyWeChat\Foundation\Application(config('wechat'));
+	    $oauth = $wechat->oauth;
+	    
+	    if (empty($_SESSION['wechat_user'])) {
+		    $_SESSION['target_url'] = 'user/profile';
+		    return $oauth->redirect();
+	    }
+	    
+	    $user = $_SESSION['wechat_user'];
+	    
     	return view('hello');
     }
 }
