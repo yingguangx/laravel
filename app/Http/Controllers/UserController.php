da7aa6619e26916c16752415527487ea96744c63
zyg <838509563@qq.com>
2017-08-31 13:41:04 +0800

    Fix user
    Fix user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,13 +7,15 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends BaseController
 {
     public function index(){
-	    $user = session('wechat.oauth_user'); // 拿到授权用户资料
+//	    $user = session('wechat.oauth_user'); // 拿到授权用户资料
 	
-	    dd($user);
+//	    dd($user);
+	    dd(Auth::user());
     	return view('user.user');
 //	    $wechat = new \EasyWeChat\Foundation\Application(config('wechat'));
 //	    $oauth = $wechat->oauth;
