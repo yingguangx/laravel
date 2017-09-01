ffba5eda50482bf5c0fd7c1e9744d9699f02e3f0
zyg <838509563@qq.com>
2017-09-01 10:11:35 +0800

    Fix

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Route::get('/', function () {
 	return view('welcome');
 });
 
+
 //Route::group(['middleware' => ['web', 'wechat.oauth']], function () {
 Route::group(['middleware' => ['auth']], function () {
 	Route::get('/home', 'HomeController@index')->name('home');
