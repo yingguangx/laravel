906022f246d1e78fe7eafe1aec45bf918da9c738
sentimentalcs <310256023@qq.com>
2017-09-04 19:49:32 +0800

    add files

--- app/Http/Controllers/pointsController.php
+++ app/Http/Controllers/pointsController.php
@@ -9,6 +9,7 @@ use App\Models\user_coupon;
 use App\Models\Wheel_setting;
 use Illuminate\Console\Application;
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
@@ -46,7 +47,15 @@ class pointsController extends Controller
     public function get_award()
     {
         $award_arr = $this->award_list();
+        $prize_detail = Prize_detail::find(Prize_detail::get_award());
         $data = Prize_detail::format_award_arr(Prize_detail::award_list());
-        return MyWoKer::jsonSuccess(array_search(Prize_detail::find(Prize_detail::get_award())->name,$data['restaraunts'])+1);
+        if(!$prize_detail->name!='谢谢参与'){
+            //向中奖的数据中插入数据
+            $userCoupon = new user_coupon([
+//                'user_id' =>
+            ]);
+        }
+
+        return MyWoKer::jsonSuccess(array_search($prize_detail->name,$data['restaraunts'])+1);
     }
 }
