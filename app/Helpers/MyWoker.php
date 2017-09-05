8762a49067ebf7e92b92d4a3edd963c740a2eeca
sentimentalcs <310256023@qq.com>
2017-09-05 16:54:07 +0800

    大转盘功能完成

--- app/Helpers/MyWoker.php
+++ app/Helpers/MyWoker.php
@@ -14,6 +14,7 @@ class MyWoker
 
     const CODE_SUC = 200;
     const CODE_ERR = 400;
+
     /*根据各个奖品的中奖几率取出中奖的物品
      * @param $award
      * @return int|string
@@ -31,6 +32,28 @@ class MyWoker
         }
         return $result;
     }
+
+    /**
+     * @param int $time              时间戳 默认为当前时间
+     * @param $delimiter             时间分隔符 默认为-
+     * @param $hms                   是否精确到时分秒
+     * @return false|string
+     */
+    public static function format_time($time,$delimiter='-',$hms=false)
+    {
+        if(empty($hms)){
+            return date('Y'.$delimiter.'m'.$delimiter.'d',$time);
+        }
+        return date('Y'.$delimiter.'m'.$delimiter.'d'.' H:m:s',$time);
+    }
+
+
+    public static function get_deadline($start_time,$expire_day)
+    {
+        $ctime_begin = mktime(0,0,0,date('m',$start_time),date('d',$start_time),date('Y',$start_time));
+        return $ctime_begin + ($expire_day+1)*3600*24;
+    }
+
 
     public static function jsonSuccess($data,$code='',$message=null)
     {
--- app/Http/Controllers/pointsController.php
+++ app/Http/Controllers/pointsController.php
@@ -6,6 +6,7 @@ use App\Helpers\MyWoker;
 use App\Models\Prize;
 use App\Models\Prize_detail;
 use App\Models\user_coupon;
+use App\Models\user_play;
 use App\Models\Wheel_setting;
 use Illuminate\Console\Application;
 use Illuminate\Foundation\Bus\DispatchesJobs;
@@ -26,12 +27,22 @@ class pointsController extends Controller
      */
     public function wheel()
     {
+        //判断该大转盘活动是否开启
+        if(!Wheel_setting::is_open()){
+            return view('user.wheel',[
+                    'award_list'=> [],
+                    'rules'     => '',
+                    'luck_list' => [],
+                ]
+            );
+        }
         return view('user.wheel',[
                'award_list'=> Prize_detail::award_list(),
                'rules'     => Wheel_setting::first(['rules'])->toArray()['rules'],
                'luck_list' => user_coupon::luck_list(),
             ]
         );
+
     }
 
     /**奖品列表获取
@@ -39,6 +50,10 @@ class pointsController extends Controller
      */
     public function award_list()
     {
+        //判断该大转盘活动是否开启
+        if(!Wheel_setting::is_open()){
+            return MyWoker::jsonFail('','','该活动有效时间是'.Wheel_setting::get_act_time().',已经停止,敬请期待');
+        }
         return MyWoker::jsonSuccess(Prize_detail::format_award_arr(Prize_detail::award_list()));
     }
 
@@ -47,22 +62,26 @@ class pointsController extends Controller
      */
     public function get_award()
     {
-        $award_arr = $this->award_list();
-        if($setting = Wheel_setting::first()){
-           //判断次数是否超过了每人每天可玩的次数限制
-           dd($setting);
+        //判断该大转盘活动是否开启
+        if(!Wheel_setting::is_open()){
+            return MyWoker::jsonFail('','','该活动有效时间是'.Wheel_setting::get_act_time().',已经停止,敬请期待');
+        }
+
+        //判断该用户当天是否可玩大转盘
+        if(!user_play::can_play(Auth::user()->id)){
+            return MyWoker::jsonFail('','','今天抽奖次数已经用完，明天再来哦!');
         }
         $prize_detail_id = Prize_detail::get_award();
         $prize_detail = Prize_detail::find($prize_detail_id);
         $data = Prize_detail::format_award_arr(Prize_detail::award_list());
         $userCoupon = '';
-        if(!$prize_detail->name!='谢谢参与'){
+        if($prize_detail->name!='谢谢参与'){
             //向中奖的数据中插入数据
             $userCoupon = new user_coupon([
                 'user_id'         => Auth::user()->id,
                 'prize_detail_id' => $prize_detail_id,
                 'create_time'     => time(),
-                'expire_time'     => time()+100000000,
+                'expire_time'     => MyWoker::get_deadline(time(),Wheel_setting::first()->valid_time),
             ]);
            $userCoupon->save();
            $userCoupon = $userCoupon::luck_list($userCoupon->id);
