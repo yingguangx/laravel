0c10b7dea7c4badefea210bf25c7568f9ae6d07d
zhou <310256023@qq.com>
2017-09-06 00:10:43 +0800

    卡券列表获取

--- app/Models/user_coupon.php
+++ app/Models/user_coupon.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Helpers\MyWoker;
 use Illuminate\Database\Eloquent\Model;
 
 class user_coupon extends Model
@@ -23,5 +24,38 @@ class user_coupon extends Model
                    -> toArray();
         }
         return $query->where($table_name.'.id','=',$id)->first()->toArray();
+    }
+
+    /**获得用户卡券列表也的数据
+     * @param $user_id
+     * @return array
+     */
+    public static function coupons_list($user_id)
+    {
+        $base_field = ['user_id','create_time','expire_time','pd.name'];
+        $table_name = (new self)->getTable();
+        $query = self::where('user_id','=',$user_id)
+                ->orderBy('create_time','desc')
+                ->join(Prize_detail::tableName().' as pd','pd.prize_detail_id','=',$table_name.'.prize_detail_id');
+        ;
+        $un_used = (clone $query)->where('is_used','=',0);
+        $used_query = (clone $query)->where('is_used','=','1');
+
+        return [
+          'un_used'=>[
+              'count' => $un_used->count(),
+              'list'  => $un_used->get($base_field)->transform(function($item,$key){
+                    $item['time'] = MyWoker::format_time($item['create_time']).' 至 '.MyWoker::format_time($item['expire_time']);
+                    return $item;
+              })->toArray(),
+          ],
+          'used'=>[
+              'count' => $used_query->count(),
+              'list'  => $used_query->get($base_field)->transform(function($item,$key){
+                  $item['time'] = MyWoker::format_time($item['create_time']).' 至 '.MyWoker::format_time($item['expire_time']);
+                  return $item;
+              })->toArray(),
+          ]
+        ];
     }
 }
