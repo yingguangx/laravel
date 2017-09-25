f4ffcd9c33ff887694b900cf3b2535651f038e31
zhou <310256023@qq.com>
2017-09-25 23:44:23 +0800

    使用卡券修复

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     protected $table = 'order';
-    public $fillable = ['game_id','game_account','money','value','user_id'];
+    public $fillable = ['game_id','game_account','money','value','user_id','type'];
 }
