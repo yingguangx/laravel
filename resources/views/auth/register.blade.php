8af56e1e82d6ef21a8a27c63f183f63ad7c82015
yechunguo <1297742957@qq.com>
2017-09-30 15:22:54 +0800

    xiugai

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -24,6 +24,20 @@
                             </div>
                         </div>
 
+                         <div class="form-group{{ $errors->has('nickName') ? ' has-error' : '' }}">
+                            <label for="nickName" class="col-md-4 control-label">nickName</label>
+
+                            <div class="col-md-6">
+                                <input id="nickName" type="text" class="form-control" name="nickName" value="{{ old('nickName') }}" required autofocus>
+
+                                @if ($errors->has('nickName'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('nickName') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
